85aaefd48015efec20663378a23531f8969a40d8
evancchow <echow@princeton.edu>
2015-05-05 13:31:27 -0400

    some sample code to show getting stack of 10 images from the game

--- convnet.py
+++ convnet.py
@@ -7,10 +7,23 @@
 # 
 ############################################################
 
-import Image
+import Image, time
 import process_image
 
-# Save recreated image: QA to make sure the img -> array works
-currscreen = process_image.screengrab()
-img = Image.fromarray(currscreen)
-img.save('recreated.png')
+# run screengrab once to bring the game window to the foreground
+process_image.screengrab()
+
+# Grab a stack of 10 screens, each 1/10 of a second apart
+# You can display these to make sure you grabbed them correctly (shows
+# actual movement of the board)
+screens = []
+for i in xrange(10):
+    screens.append(process_image.screengrab())
+    time.sleep(0.1)
+
+# save images
+for i in xrange(10):
+    img = Image.fromarray(screens[i])
+    img.save('screengrab{}.png'.format(i))
+import code
+code.interact(local=locals())
